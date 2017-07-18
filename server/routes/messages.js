1b439860c7ebfdf903dceacd2b0a0c448752441f
Kirk Rohani <kirk.rohani@gmail.com>
2017-07-18 11:50:00 -0700

    Working on compose message

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,25 +1,19 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const middleware = require('../middleware');
 const MessageController = require('../controllers').Messages;
 
-router.route('/')
-  .get(MessageController.getAll)
-  // .post(MessageController.create)
-;
 
-router.route('/:id')
-  .get((req, res) => {
+router.route('/')
+  .get( middleware.auth.verify, (req, res) => {
+    console.log('Inside Routes messages.js /api/messages');
     MessageController.getOne(req, res);
-  })
-//   .get(MessageController.getOne)  
-//   .put(MessageController.update)
-//   // .delete(MessageController.deleteOne)
-;
+  });
 
 router.route('/:id/:thread')
 .get((req, res) => {
   MessageController.getThread(req, res);
-})
+});
 
 module.exports = router;
